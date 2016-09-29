56fe164d38dcc99130c60ab84fc88787418a910b
Quinn Zepeda <developerquinnz@gmail.com>
2016-09-29 12:59:53 -0700

    added description
    moved to repo dir

---
+++ range_exer_43.py
@@ -0,0 +1,11 @@
+my_num_list = [0,1,2,3]
+my_num_list_len = len(my_num_list)
+for i in range (0, my_num_list_len):
+    print (my_num_list[i])
+
+for i in range (3,-1,-1):
+    print i
+
+for i in range(8,0,-2):
+    print i
+    
